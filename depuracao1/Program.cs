70103787600b8677132805ed1c0bd80546f14d9c
Marcos <lm@ig.com>
2012-08-16 21:36:51 -0300

    comitada divisao

--- depuracao1/Program.cs
+++ depuracao1/Program.cs
@@ -7,6 +7,10 @@ namespace depuracao1
 {
     class Program
     {
+        static float Dividir(int n, int d)
+        {
+            return n / d;
+        }
         static int Mudar(int m)
         {
             m = m + 1;
@@ -20,7 +24,18 @@ namespace depuracao1
             Console.WriteLine("O numero escolhido é: {0}", numero);
             numero = Mudar(numero);
             Console.WriteLine("O numero modificado é: {0}", numero);
+            
+
+            int denominador;
+            denominador = int.Parse(Console.ReadLine());
+            if (denominador == 0)
+            {
+                Console.WriteLine("Não exite divisão por zero");
+                denominador = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("O quociente entre {0} e {1} é {2}", numero, denominador, Dividir(numero, denominador));
             Console.Read();
+
 
         }
     }
